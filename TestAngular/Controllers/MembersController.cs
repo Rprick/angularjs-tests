0fa9334987aa42c7449d3092a593ed0d66899d3f
rprick <rprick@gmail.com>
2013-02-14 22:25:50 +0100

    Initial version

---
+++ TestAngular/Controllers/MembersController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TestAngular.Controllers
+{
+    public class MembersController : Controller
+    {
+        //
+        // GET: /Members/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        public ActionResult GetMembers() 
+        {
+            var memberResult = new MemberResult
+            {
+                members = new List<Member>
+            {
+                new Member {name="Ramon"},
+                new Member {name="Mirjana"},
+                new Member {name="Manon"},
+                new Member {name="Charlotte"}
+            }
+            };
+            return Json(memberResult, JsonRequestBehavior.AllowGet);
+        }
+    }
+
+    public class MemberResult {
+        public IEnumerable<Member> members { get; set; }
+    }
+
+    public class Member {
+        public string name { get; set; }
+        public bool inEditMode { get; set; }
+    }
+}
---
+++ TestAngular/Scripts/members.js
@@ -0,0 +1,50 @@
+﻿"use strict";
+
+function MemberController($scope) {
+    $scope.mode = "Browse";
+
+    $scope.members = [];
+    $scope.inBrowseMode = function (member) {
+        return $scope.mode === "Browse";
+    }
+
+    $scope.inEditMode = function (member) {
+        return $scope.mode === "Edit";
+    }
+
+     $scope.reset = function () {
+         $.get('members/getmembers', function (data) {
+             $scope.members = data.members;
+             $scope.mode = "Browse";
+             $scope.$apply();
+        });
+     }
+
+     $scope.reset();
+
+    $scope.memberText = "Nieuw";
+
+    $scope.totalMembers = function () {
+        return $scope.members.length;
+    }
+
+    $scope.startEdit = function (member) {
+        member.originalName = member.name;
+        member.inEditMode = true;
+    };
+
+    $scope.saveMember = function (member) {
+        member.inEditMode = false;
+        member.originalName = null;
+    };
+
+    $scope.cancelMember = function (member) {
+        member.name = member.originalName;
+        member.inEditMode = false;
+        member.originalName = null;
+    };
+
+    $scope.addMember = function () {
+        $scope.members.push({ name: $scope.memberText });
+    };
+}
